396b5221f86cd98538282cef87f6c46f88ad7281
Shivam Shukla <shukla.shivamv@gmail.com>
2025-03-22 21:55:03 +0530

    Enroll Page With Registration, Login, Forget password, for parent, admin, and student with all functionality updated

---
+++ client/src/pages/index.jsx
@@ -0,0 +1,7 @@
+import Home from "./Home/Home"
+import AboutUs from "./About/About"
+import ContactPage from "./Contact/Contact"
+import Enroll from "./Enroll/Enroll"
+import Academics from "./Academics/Academics"
+
+export { Home, AboutUs, Enroll, ContactPage, Academics }
